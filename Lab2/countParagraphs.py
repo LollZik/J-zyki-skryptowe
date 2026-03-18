85ba278ebf10368dd29ca7ca6a16d8b2eead8587
Othalax <dominisiajarzabek@gmail.com>
2026-03-18 23:26:18 +0100

    Added search functions

--- Lab2/countParagraphs.py
+++ Lab2/countParagraphs.py
@@ -1,20 +1,16 @@
 import sys
 
 def main():
-    text = sys.stdin.read()
-    if text:
-        paragraphs = 0
-        inParagraph = False
-        for line in text.splitlines():
-            if line.strip() == "":
-                in_para = False
-            else:
-                if not in_para:
-                    paragraphs += 1
-                    inParagraph = True
-        print(paragraphs)
-    else:
-        print("0")
+    paragraphs = 0
+    inParagraph = False
+    for line in sys.stdin:
+        if line.strip() == "":
+            inParagraph = False
+        else:
+            if not inParagraph:
+                paragraphs += 1
+                inParagraph = True
+    print(paragraphs)
 
 if __name__ == '__main__':
     main()
